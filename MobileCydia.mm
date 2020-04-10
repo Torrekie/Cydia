42f0c28ce2b2800a3e93bd7cadd242d5b8a55175
Sam Bingner <sam@bingner.com>
2020-04-10 13:51:07 -1000

    Fix my patches to compile for armv6 - still needs work

--- MobileCydia.mm
+++ MobileCydia.mm
@@ -2907,7 +2907,11 @@ struct PackageNameOrdering :
                 return @"REINSTALL";
             else*/ switch (state.Status) {
                 case -1:
+#ifndef __arm__
                     return [database_ cache].Policy->GetCandidateVer(iterator_)==state.CandidateVerIter([database_ cache])?@"UPGRADE":@"DOWNGRADE";
+#else
+                    return @"DOWNGRADE";
+#endif
                 case 0:
                     return @"INSTALL";
                 case 1:
@@ -4866,7 +4870,11 @@ bool DepSubstrate(const pkgCache::VerIterator &iterator) {
             else if (!state.Delete() && (state.iFlags & pkgDepCache::ReInstall) == pkgDepCache::ReInstall)
                 [reinstalls addObject:name];
             // XXX: move before previous if
+#ifndef __arm__
             else if (state.Upgrade() || (state.Downgrade() && state.CandidateVerIter(cache) == cache.Policy->GetCandidateVer(iterator)))
+#else
+            else if (state.Upgrade())
+#endif
                 [upgrades addObject:name];
             else if (state.Downgrade())
                 [downgrades addObject:name];
