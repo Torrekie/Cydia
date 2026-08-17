8ee0f66b899dac0e01f3b00697bceabe25c6aaa3
Torrekie <31795847+Torrekie@users.noreply.github.com>
2026-08-18 00:28:23 +0800

    refactor: extract shared Cydia value types

---
+++ Cydia/CYString.hpp
@@ -0,0 +1,158 @@
+#ifndef Cydia_CYString_HPP
+#define Cydia_CYString_HPP
+
+#include "CyteKit/UCPlatform.h"
+#include "Menes/Pooling.hpp"
+
+#include <CoreFoundation/CoreFoundation.h>
+#include <Foundation/Foundation.h>
+
+#include <cstring>
+#include <functional>
+#include <string>
+
+static _finline CFStringRef CYStringCreate(const char *data, size_t size) {
+    return size == 0 ? NULL :
+        CFStringCreateWithBytesNoCopy(kCFAllocatorDefault, reinterpret_cast<const uint8_t *>(data), size, kCFStringEncodingUTF8, NO, kCFAllocatorNull) ?:
+        CFStringCreateWithBytesNoCopy(kCFAllocatorDefault, reinterpret_cast<const uint8_t *>(data), size, kCFStringEncodingISOLatin1, NO, kCFAllocatorNull);
+}
+
+static _finline CFStringRef CYStringCreate(const std::string &data) {
+    return CYStringCreate(data.data(), data.size());
+}
+
+static _finline CFStringRef CYStringCreate(const char *data) {
+    return CYStringCreate(data, strlen(data));
+}
+
+class CYString {
+  private:
+    char *data_;
+    size_t size_;
+    CFStringRef cache_;
+
+    _finline void clear_() {
+        if (cache_ != NULL) {
+            CFRelease(cache_);
+            cache_ = NULL;
+        }
+    }
+
+  public:
+    _finline bool empty() const {
+        return size_ == 0;
+    }
+
+    _finline size_t size() const {
+        return size_;
+    }
+
+    _finline char *data() const {
+        return data_;
+    }
+
+    _finline void clear() {
+        size_ = 0;
+        clear_();
+    }
+
+    _finline CYString() :
+        data_(0),
+        size_(0),
+        cache_(NULL)
+    {
+    }
+
+    _finline ~CYString() {
+        clear_();
+    }
+
+    void operator =(const CYString &rhs) {
+        data_ = rhs.data_;
+        size_ = rhs.size_;
+
+        if (rhs.cache_ == nil)
+            cache_ = NULL;
+        else
+            cache_ = reinterpret_cast<CFStringRef>(CFRetain(rhs.cache_));
+    }
+
+    void copy(CYPool *pool) {
+        char *temp(pool->malloc<char>(size_ + 1));
+        memcpy(temp, data_, size_);
+        temp[size_] = '\0';
+        data_ = temp;
+    }
+
+    void set(CYPool *pool, const char *data, size_t size) {
+        if (size == 0)
+            clear();
+        else {
+            clear_();
+
+            data_ = const_cast<char *>(data);
+            size_ = size;
+
+            if (pool != NULL)
+                copy(pool);
+        }
+    }
+
+    _finline void set(CYPool *pool, const char *data) {
+        set(pool, data, data == NULL ? 0 : strlen(data));
+    }
+
+    _finline void set(CYPool *pool, const std::string &rhs) {
+        set(pool, rhs.data(), rhs.size());
+    }
+
+    bool operator ==(const CYString &rhs) const {
+        return size_ == rhs.size_ && memcmp(data_, rhs.data_, size_) == 0;
+    }
+
+    _finline operator CFStringRef() {
+        if (cache_ == NULL)
+            cache_ = CYStringCreate(data_, size_);
+        return cache_;
+    }
+
+    _finline operator id() {
+        return (NSString *) static_cast<CFStringRef>(*this);
+    }
+
+    _finline operator const char *() {
+        return reinterpret_cast<const char *>(data_);
+    }
+};
+
+extern "C" {
+    CF_EXPORT CFHashCode CFStringHashNSString(CFStringRef str);
+}
+
+struct NSStringMapHash :
+    std::unary_function<NSString *, size_t>
+{
+    _finline size_t operator ()(NSString *value) const {
+        return CFStringHashNSString((CFStringRef) value);
+    }
+};
+
+struct NSStringMapLess :
+    std::binary_function<NSString *, NSString *, bool>
+{
+    _finline bool operator ()(NSString *lhs, NSString *rhs) const {
+        return [lhs compare:rhs] == NSOrderedAscending;
+    }
+};
+
+struct NSStringMapEqual :
+    std::binary_function<NSString *, NSString *, bool>
+{
+    _finline bool operator ()(NSString *lhs, NSString *rhs) const {
+        return CFStringCompare((CFStringRef) lhs, (CFStringRef) rhs, 0) == kCFCompareEqualTo;
+        //CFEqual((CFTypeRef) lhs, (CFTypeRef) rhs);
+        //[lhs isEqualToString:rhs];
+    }
+};
+
+#endif//Cydia_CYString_HPP
