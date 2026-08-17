4ef0c1c8e58b454673f9da035e6c7a54d7e2ef1b
Torrekie <31795847+Torrekie@users.noreply.github.com>
2026-08-18 00:35:18 +0800

    refactor: extract package relation models

---
+++ Cydia/Relations.h
@@ -0,0 +1,45 @@
+#ifndef Cydia_Relations_H
+#define Cydia_Relations_H
+
+#include "CyteKit/UCPlatform.h"
+#include "Menes/ObjectHandle.h"
+
+#include <Foundation/Foundation.h>
+
+#include <apt-pkg/pkgcache.h>
+
+@interface CydiaOperation : NSObject {
+    _H<NSString> operator_;
+    _H<NSString> value_;
+}
+
+- (id) initWithOperator:(const char *)_operator value:(const char *)value;
+- (NSString *) operator;
+- (NSString *) value;
+
+@end
+
+@interface CydiaClause : NSObject {
+    _H<NSString> package_;
+    _H<CydiaOperation> version_;
+}
+
+- (id) initWithIterator:(pkgCache::DepIterator &)dep;
+- (NSString *) package;
+- (CydiaOperation *) version;
+
+@end
+
+@interface CydiaRelation : NSObject {
+    _H<NSString> relationship_;
+    _H<NSMutableArray> clauses_;
+}
+
+- (id) initWithIterator:(pkgCache::DepIterator &)dep;
+- (NSString *) relationship;
+- (NSArray *) clauses;
+- (void) addClause:(CydiaClause *)clause;
+
+@end
+
+#endif//Cydia_Relations_H
---
+++ Cydia/Relations.mm
@@ -0,0 +1,124 @@
+#include "Cydia/Relations.h"
+
+@implementation CydiaOperation
+
+- (id) initWithOperator:(const char *)_operator value:(const char *)value {
+    if ((self = [super init]) != nil) {
+        operator_ = [NSString stringWithUTF8String:_operator];
+        value_ = [NSString stringWithUTF8String:value];
+    } return self;
+}
+
++ (NSArray *) _attributeKeys {
+    return [NSArray arrayWithObjects:
+        @"operator",
+        @"value",
+    nil];
+}
+
+- (NSArray *) attributeKeys {
+    return [[self class] _attributeKeys];
+}
+
++ (BOOL) isKeyExcludedFromWebScript:(const char *)name {
+    return ![[self _attributeKeys] containsObject:[NSString stringWithUTF8String:name]] && [super isKeyExcludedFromWebScript:name];
+}
+
+- (NSString *) operator {
+    return operator_;
+}
+
+- (NSString *) value {
+    return value_;
+}
+
+@end
+
+@implementation CydiaClause
+
+- (id) initWithIterator:(pkgCache::DepIterator &)dep {
+    if ((self = [super init]) != nil) {
+        package_ = [NSString stringWithUTF8String:dep.TargetPkg().Name()];
+
+        if (const char *version = dep.TargetVer())
+            version_ = [[[CydiaOperation alloc] initWithOperator:dep.CompType() value:version] autorelease];
+        else
+            version_ = (id) [NSNull null];
+    } return self;
+}
+
++ (NSArray *) _attributeKeys {
+    return [NSArray arrayWithObjects:
+        @"package",
+        @"version",
+    nil];
+}
+
+- (NSArray *) attributeKeys {
+    return [[self class] _attributeKeys];
+}
+
++ (BOOL) isKeyExcludedFromWebScript:(const char *)name {
+    return ![[self _attributeKeys] containsObject:[NSString stringWithUTF8String:name]] && [super isKeyExcludedFromWebScript:name];
+}
+
+- (NSString *) package {
+    return package_;
+}
+
+- (CydiaOperation *) version {
+    return version_;
+}
+
+@end
+
+@implementation CydiaRelation
+
+- (id) initWithIterator:(pkgCache::DepIterator &)dep {
+    if ((self = [super init]) != nil) {
+        relationship_ = [NSString stringWithUTF8String:dep.DepType()];
+        clauses_ = [NSMutableArray arrayWithCapacity:8];
+
+        pkgCache::DepIterator start;
+        pkgCache::DepIterator end;
+        dep.GlobOr(start, end); // ++dep
+
+        _forever {
+            [clauses_ addObject:[[[CydiaClause alloc] initWithIterator:start] autorelease]];
+
+            // yes, seriously. (wtf?)
+            if (start == end)
+                break;
+            ++start;
+        }
+    } return self;
+}
+
++ (NSArray *) _attributeKeys {
+    return [NSArray arrayWithObjects:
+        @"clauses",
+        @"relationship",
+    nil];
+}
+
+- (NSArray *) attributeKeys {
+    return [[self class] _attributeKeys];
+}
+
++ (BOOL) isKeyExcludedFromWebScript:(const char *)name {
+    return ![[self _attributeKeys] containsObject:[NSString stringWithUTF8String:name]] && [super isKeyExcludedFromWebScript:name];
+}
+
+- (NSString *) relationship {
+    return relationship_;
+}
+
+- (NSArray *) clauses {
+    return clauses_;
+}
+
+- (void) addClause:(CydiaClause *)clause {
+    [clauses_ addObject:clause];
+}
+
+@end
